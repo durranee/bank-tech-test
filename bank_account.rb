1e28e651216d5451d55db52117c0fe2837585147
durranee <durranee@hotmail.com>
2018-04-23 15:51:22 +0100

    user can deposit the ammount

--- bank_account.rb
+++ bank_account.rb
@@ -1,7 +1,13 @@
 class BankAccount
   attr_reader :statement, :balance
+
   def initialize
     @balance = 0
     @statement = [[]]
+  end
+
+  def deposit(date, credit, debit = nil)
+    @balance += credit
+    @statement.push([date, credit, debit, @balance])
   end
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,8 +1,27 @@
 require './bank_account'
 
-describe "#new" do
-  it "expects balance to be 0 at the start" do
-    account = BankAccount.new
-    expect(account.balance).to eq(0)
+describe "BankAccount" do
+
+  before(:each) do
+    @account = BankAccount.new
+  end
+
+  describe "#new" do
+    it "expects balance to be 0 at the start" do
+      expect(@account.balance).to eq(0)
+    end
+  end
+
+  describe "#deposit" do
+
+    it "expects to add the amount given to balance" do
+      @account.deposit("20/02/2017", 1000)
+      expect(@account.balance).to eq(1000)
+    end
+
+    it "expects to create a transaction history" do
+      @account.deposit("20/02/2017", 1000)
+      expect(@account.statement).to include(["20/02/2017", 1000, nil, 1000])
+    end
   end
 end
