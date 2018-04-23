7dd01b3bebc4f15d207d16b04a6632fded35071a
durranee <durranee@hotmail.com>
2018-04-23 18:05:48 +0100

    refactored code

--- bank_account.rb
+++ bank_account.rb
@@ -1,4 +1,6 @@
 require 'time'
+
+# Bank account class
 class BankAccount
   attr_reader :statement, :balance
 
@@ -13,7 +15,7 @@ class BankAccount
   end
 
   def debit(date, amount)
-    raise "Insufficient balance: Transaction declined." if @balance < amount
+    raise 'Insufficient balance: Transaction declined.' if @balance < amount
     @balance -= amount
     @statement.push([Time.parse(date), nil, amount, @balance])
   end
@@ -21,20 +23,21 @@ class BankAccount
   def print_statement
     print_header
     statement.each do |transaction|
-      print "#{transaction[0].strftime("%d/%m/%Y").center(10)} || "
+      print "#{transaction[0].strftime('%d/%m/%Y').center(12)} || "
       print "#{transaction[1].to_s.rjust(10)} || "
       print "#{transaction[2].to_s.rjust(10)} || "
-      print "#{transaction[3].to_s.rjust(10)}"
-      puts ""
+      print transaction[3].to_s.rjust(10).to_s
+      puts ''
     end
   end
 
   private
+
   def print_header
-    print "Date".center(10) + " || "
-    print "Credit".center(10) + " || "
-    print "Debit".center(10) + " || "
-    print "Balance".center(10)
-    puts ""
+    print 'Date'.center(12) + ' || '
+    print 'Credit'.center(10) + ' || '
+    print 'Debit'.center(10) + ' || '
+    print 'Balance'.center(10)
+    puts ''
   end
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,60 +1,65 @@
 require 'time'
 require './bank_account'
 
-describe "BankAccount" do
-
+describe 'BankAccount' do
   before(:each) do
     @account = BankAccount.new
   end
 
-  describe "#new" do
-    it "expects balance to be 0 at the start" do
+  describe '#new' do
+    it 'expects balance to be 0 at the start' do
       expect(@account.balance).to eq(0)
     end
   end
 
-  describe "#credit" do
-
-    it "expects to add the amount given to balance" do
-      @account.credit("20/02/2017", 1000)
+  describe '#credit' do
+    it 'expects to add the amount given to balance' do
+      @account.credit('20/02/2017', 1000)
       expect(@account.balance).to eq(1000)
     end
 
-    it "expects to create a transaction history" do
-      @account.credit("20/02/2017", 1000)
-      expect(@account.statement).to include([Time.parse("20/02/2017"), 1000, nil, 1000])
+    it 'expects to create a transaction history' do
+      @account.credit('20/02/2017', 1000)
+      expect(@account.statement).to include(
+        [Time.parse('20/02/2017'), 1000, nil, 1000]
+      )
     end
   end
 
-  describe "#debit" do
-    it "expects to reduce the balance by ammount provided" do
-      @account.credit("20/02/2017", 1000)
-      @account.debit("21/02/2017", 100)
+  describe '#debit' do
+    it 'expects to reduce the balance by ammount provided' do
+      @account.credit('20/02/2017', 1000)
+      @account.debit('21/02/2017', 100)
       expect(@account.balance).to eq(900)
     end
 
-    it "expects to create a transaction history" do
-      @account.credit("21/02/2017", 1000)
-      @account.debit("22/02/2017", 100)
-      expect(@account.statement).to include([Time.parse("22/02/2017"), nil, 100, 900])
+    it 'expects to create a transaction history' do
+      @account.credit('21/02/2017', 1000)
+      @account.debit('22/02/2017', 100)
+      expect(@account.statement).to include(
+        [Time.parse('22/02/2017'), nil, 100, 900]
+      )
     end
 
-    it "expects to raise error if withdrawing amount is more than balance" do
-      expect{@account.debit("22/02/2017", 100)}.to raise_error("Insufficient balance: Transaction declined.")
+    it 'expects to raise error if withdrawing amount is more than balance' do
+      expect { @account.debit('22/02/2017', 100) }.to raise_error('Insufficient balance: Transaction declined.')
     end
   end
 
-  describe "#print_statement" do
-    context "should print transaction body" do
-      specify {
-        @account.credit("21/02/2017", 1000)
-        @account.debit("22/02/2017", 100)
+  describe '#print_statement' do
+    context 'should print transaction body' do
+      specify do
+        @account.credit('21/02/2017', 1000)
+        @account.debit('22/02/2017', 100)
         expect { @account.print_statement }.to output(
-          "#{"Date".center(10)}" + " || #{"Credit".center(10)} || " +
-          "#{"Debit".center(10)} || " + "#{"Balance".center(10)}\n" +
-          "#{"21/02/2017".center(10)} || #{"1000".rjust(10)} || #{"".rjust(10)} || #{"1000".rjust(10)}\n" +
-          "#{"22/02/2017".center(10)} || #{"".rjust(10)} || #{"100".rjust(10)} || #{"900".rjust(10)}\n").to_stdout }
+          'Date'.center(12) + ' || ' + 'Credit'.center(10) + ' || ' +
+          'Debit'.center(10) + ' || ' + 'Balance'.center(10) + "\n" +
+          '21/02/2017'.center(12) + ' || ' + '1000'.rjust(10) + ' || ' +
+          ''.rjust(10) + ' || ' + '1000'.rjust(10) + "\n" +
+          '22/02/2017'.center(12) + ' || ' + ''.rjust(10) + ' || ' +
+          '100'.rjust(10) + ' || ' + '900'.rjust(10) + "\n"
+        ).to_stdout
+      end
     end
-
   end
 end
